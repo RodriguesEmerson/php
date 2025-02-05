e802e52a6fc2881210f44becf60a33524f3b5cc2
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-02-05 19:13:50 -0300

    Curso PHP moderno - Guanabara, Finalizado

---
+++ ex008/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>First Site in PHP</title>
+</head>
+<body>
+   <header>
+      <h1>First Sit in PHP</h1>
+   </header>
+   <main>
+      
+   </main>
+
+</body>
+</html>
