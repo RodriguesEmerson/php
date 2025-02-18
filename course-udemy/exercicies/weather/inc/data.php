3fdb8b7d6bd8e5b81b93b1691fafbe8dc7b8dc5e
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-02-18 20:24:09 -0300

    Função formata data Criada. Mudei a API

--- course-udemy/exercicies/weather/inc/data.php
+++ course-udemy/exercicies/weather/inc/data.php
@@ -1,15 +1,28 @@
 <?php 
-   // Carregar variáveis do .env
+   // Carrega variáveis do .env
    require_once  './../../../config.php';
    loadEnv();
    function getData(){
      
       $apiKey = getenv('WEATHER_KEY'); // Obtém a chave do ambiente;
       $city_name = $_GET["city"] ?? "Brasília";
-      $curl = "http://api.openweathermap.org/data/2.5/forecast?q=" . urlencode($city_name) . "&appid=" . $apiKey;
+      $curl = "http://api.weatherapi.com/v1/forecast.json?key=" . $apiKey . "&q=" . urlencode($city_name) . "&days=3&aqi=no&alerts=no";
       $response = file_get_contents($curl);
       $data = json_decode($response, true);
 
       return($data);
+   };
+
+   function formatedDate(string $date): array{
+      try { 
+         $formatedDate = new DateTime($date);
+         return [
+            "day" => $formatedDate->format("d-m-Y"),
+            "hour" => $formatedDate->format("H:i")
+         ];
+      } catch (Exception $e) {
+         return ["error" => "Data inválida"];
+      }
    }
+   
 ?>
--- course-udemy/exercicies/weather/index.php
+++ course-udemy/exercicies/weather/index.php
@@ -12,7 +12,25 @@
       if(!$data) {
          die("Erro: Dados não encontrados.");
       }
-      var_dump($data)
+      var_dump($data);
+      // var_dump($data['list'][0]);
+      $city = $data['location']['name'];
+      $country = $data['location']['country'];
+      $date = formatedDate($data['location']['localtime']);
+      $curr_temp = $data['current']['temp_c'];
+      $curr_condition = $data['current']['condition']['text'];
+      $curr_icon = $data['current']['condition']['icon'];
+      $curr_wind = $data['current']['wind_kph'];
+      $curr_humidity = $data['current']['humidity'];
+      $curr_feelslike = $data['current']['feelslike_c'];
+      $curr_vis = $data['current']['vis_km'];
+      $curr_uv = $data['current']['uv'];
+
+
    ?>
+   <?php echo $curr_icon?>
+   <p><img src="<?php echo $curr_icon ?>" alt="icon"><?php echo $curr_temp?> &deg;C</p>
+   <p>Cidade: <?php echo "$city - $country" ?></p>
+   <p><?php echo $date['hour'] . " " . $date['day']?></p>
 </body>
 </html>
