155d0273dd3fe4a095b821e552bae76ecdf4f7cb
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-02-25 19:57:43 -0300

    Projetc Galery with Directory Management OK

---
+++ course-udemy/exercicies/directory-management/galery.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Galery</title>
+   <style>
+
+      *{
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+         font-family: Arial, Helvetica, sans-serif;
+      }
+      header{
+         height: 50px;
+         background-color: darkcyan;
+         text-align: center;
+         align-content: center;
+         margin-bottom: 10px;
+         color: white;
+      }
+      .galery{
+         display: flex;
+         flex-direction: column;
+         gap: 5px;
+         max-width: 750px;
+         margin: auto;
+         margin-bottom: 10px;
+      }
+      .img-box{
+         min-width: 300px;
+         width: 300px;
+         overflow: hidden;
+         border-radius: 5px;
+         height: 150px;
+
+         & img{
+            max-width: 100%;
+         }
+      }
+      .item{
+         display: flex;
+         flex-direction: row;
+         align-items: center;
+         gap: 5px;
+
+         & .content{
+            & h2{
+            }
+            & p {
+               font-size: 15px;
+            }
+         }
+      }
+   </style>
+</head>
+<body>
+   <?php 
+      function s($value){
+         return htmlspecialchars($value, ENT_QUOTES, "UTF-8");
+      }
+
+      $handle = opendir(__DIR__ . '/images');
+
+      $images = [];
+      $imagesAllowedExtensions = ['jpg'];
+
+      while(($currentFile = readdir($handle) )!== false){
+         $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
+
+         if(in_array($extension, $imagesAllowedExtensions)){
+
+            //Only takes the file name
+            $imageName = pathinfo($currentFile, PATHINFO_FILENAME);
+
+            $contentText = __DIR__ . "/images/$imageName.txt";
+
+            if(file_exists($contentText)){
+               //Get the file contet
+               $content = file_get_contents($contentText);
+               
+               //Divide the content by lines
+               $lines = explode("\n", $content);
+
+               //Remove empty lines.
+               $lines = array_filter(array_map('trim', $lines));
+            }
+
+            $images[$imageName] = [
+               "img" => $currentFile,
+               "title" => isset($lines[0]) ? $lines[0] : '',
+               "description" => isset($lines[1]) ? $lines[1] : ''
+            ];
+         }
+
+      };
+
+      closedir($handle);
+   ?>
+   <header>
+      <h1>Galery</h1>
+   </header>
+   <main>
+      <section class="galery">
+         <?php foreach($images AS $image) :?>
+            <div class="item">
+               <div class="img-box">
+                  <img src="./images/<?php echo rawurlencode($image['img']) ?>" alt="<?php  echo s($image['title'])?> image">
+               </div>
+               <div class="content">
+                  <h2><?php echo s($image['title']) ?></h2>
+                  <p><?php echo s($image['description']) ?></p>
+               </div>
+            </div>
+
+            <?php endforeach;?>
+      </section>
+   </main>
+   
+</body>
+</html>
--- course-udemy/exercicies/directory-management/index.php
+++ course-udemy/exercicies/directory-management/index.php
@@ -4,13 +4,46 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Image List</title>
+
+   <style>
+      .galery{
+         display: flex;
+         flex-direction: column;
+         gap: 5px;
+      }
+      .img-box{
+         width: 300px;
+         overflow: hidden;
+         border-radius: 5px;
+         height: 150px;
+
+         & img{
+            max-width: 100%;
+         }
+      }
+
+   </style>
 </head>
 <body>
    <header>Automatic Image List</header>
    <main>
       <?php 
+         //Check if exeists the repective file in the folder;
+         var_dump(file_exists(__DIR__ . '/images/IMG-1.jpg'));
 
-         //Pega/abre a pasta com as imagens
+         //Folders are also considered file here.
+         var_dump(file_exists(__DIR__ . '/images'));
+
+         //Check if the file is a directory
+         var_dump(is_dir(__DIR__ . '/images'));
+         
+         //Check if the file is exactly a file 
+         var_dump(is_file(__DIR__ . '/images'));
+
+         //Check the file size
+         var_dump(filesize(__DIR__ . '/images/IMG-1.jpg') / 1024 / 1024);
+
+         //Ope the folther with the images.
          $handle = opendir(__DIR__ . '/images');
          
          // $currentFile = readdir($handle);
@@ -20,28 +53,41 @@
          // }
 
          $images = [];
-         //Faz um loop por cada item da pasta
+         $allowdExtensions = ['jpg', 'jpeg', 'png'];
+         //Cycle through each item in the folder.
          while(($currentFile = readdir($handle)) !== false){
-            #Pula os dois primeiro item que sempre terâo na pasta.
+            #Skipe the two firts items of the folder, they are gonna always to apear.
             if($currentFile === '.' || $currentFile === '..') continue;
 
-            //Pega a extenção/formato do arquivo;
+            //It takes the extension of the file.
             $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
-            if($extension !== 'jpg') continue;
-            var_dump($extension);
 
+            //It only takes allowed files.
+            if(!in_array($extension, $allowdExtensions)){
+               continue;
+            }
 
-            #Adiciona cada item ao array.
+            // if($extension !== 'jpg') continue;
+            // var_dump($extension);
+
+            #Push the images in the array.
             $images[] = $currentFile;
          }
 
-         //Fecha a pasta *importante
+         //Close the foldder *important
          closedir($handle);
       ?>
 
+   <section class="galery">
       <?php foreach($images AS $image) :?>
-         <img src="./images/<?php echo rawurlencode($image) ?>" alt="Image">
-      <?php endforeach;?>
+            <div class="item">
+               <div class="img-box">
+                  <img src="./images/<?php echo rawurlencode($image) ?>" alt="Image">
+               </div>
+            </div>
+
+            <?php endforeach;?>
+      </section>
    </main>
 
 </body>
