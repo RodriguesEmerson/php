358772fb2b8096baf4a26df07fd95488ba451771
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2025-03-15 23:28:42 -0300

    Project Paused, now I'm going to focous in practicing

--- course-udemy/exercicies/07-cities-explorer/src/WordCityModel.php
+++ course-udemy/exercicies/07-cities-explorer/src/WordCityModel.php
@@ -1,13 +1,18 @@
 <?php 
    class WordCityModel {
-      public string $id;
-      public string $city;
-      public float $lat;
-      public float $lng;
-      public string $country;
-      public string $iso1;
-      public string $iso2;
-      public string $capital;
-      public int $population;
+      
+      public function __construct(
+         public int $id,
+         public string $city,
+         public string $cityAscii,
+         public float $lat,
+         public float $lng,
+         public string $country,
+         public string $iso1,
+         public string $iso2,
+         public string $adminName,
+         public string $capital,
+         public int $population
+      ){}
    }
 ?>
--- course-udemy/exercicies/07-cities-explorer/src/WordCityRepository.php
+++ course-udemy/exercicies/07-cities-explorer/src/WordCityRepository.php
@@ -1,19 +1,39 @@
 <?php 
+   declare(strict_types=1);
+
    class WordCityRepository {
       
-      public function __construct(private PDO $pdo){
-      }
+      public function __construct(private PDO $pdo){}
 
       public function fetch():array{
-         $stmt = $this->pdo->prepare('SELECT `id`, `city`, `lat`, `lng`, `country`, `iso2`, `iso3`, `capital`, `population` 
+         $stmt = $this->pdo->prepare('SELECT `id`, `city`, `city_ascii`, `lat`, `lng`, `country`, `iso2`, `iso3`, `admin_name`, `capital`, `population` 
                                       FROM `worldcities` 
                                       ORDER BY `population` 
                                       LIMIT 10'
                                     );
          $stmt->execute();
          // $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-         $data = $stmt->fetchAll(PDO::FETCH_CLASS); //It brings an object notation;
-         return $data;
+         // $data = $stmt->fetchAll(PDO::FETCH_CLASS, 'WordCityModel'); //It brings an object notation;
+         $models = [];
+         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+         foreach($data AS $city){
+            $models[] = new WordCityModel(
+               $city['id'],
+               $city['city'],
+               $city['city_ascii'],
+               (float) $city['lat'],
+               (float) $city['lng'],
+               $city['country'],
+               $city['iso2'],
+               $city['iso3'],
+               $city['admin_name'],
+               $city['capital'],
+               $city['population'],
+            );
+         }
+         var_dump($models);
+         return [];
       }
    }
 ?>
